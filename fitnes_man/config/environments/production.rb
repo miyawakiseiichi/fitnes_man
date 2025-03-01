90b7c5148478a76dd9620d6be0da534fc141a53b
Seiichi Miyawaki <380keyseiichi@gmail.com>
2025-03-01 19:06:06 +0900

    デプロイ3

--- fitnes_man/config/environments/production.rb
+++ fitnes_man/config/environments/production.rb
@@ -94,7 +94,6 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
-
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
