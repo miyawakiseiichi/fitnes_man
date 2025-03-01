1f218ac0353ce3e00f7c487dd4f0b476d8becc53
Seiichi Miyawaki <380keyseiichi@gmail.com>
2025-03-02 03:13:33 +0900

    delete

--- fitnes_man/fitnes_man/bin/render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# エラー時に即終了
-set -o errexit
-
-bundle install
-yarn install
-yarn build # esbuildでJSをバンドル
-bundle exec rake assets:precompile # sprocketsでCSSをプリコンパイル
-bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile # DBマイグレーション（ridgepole使用）
