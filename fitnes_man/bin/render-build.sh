fe0944c20c46e0b2231f30b0a28f3207dcb6e73b
Seiichi Miyawaki <380keyseiichi@gmail.com>
2025-03-02 02:57:16 +0900

    deploy06

--- fitnes_man/bin/render-build.sh
+++ fitnes_man/bin/render-build.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
-# exit on error
+# エラー発生時にスクリプトを停止
 set -o errexit
 
+# Ruby & JSの依存関係をインストール
 bundle install
 yarn install
-yarn build # jsファイルをesbuildでバンドルしているため
-bundle exec rake assets:precompile # cssはsprocketsを使っているため
-bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います
+
+# フロントエンドのビルド
+yarn build  # JavaScriptをesbuildでバンドル（必要に応じて変更）
+
+# アセットのプリコンパイル
+bundle exec rake assets:precompile  # CSSをSprocketsでコンパイル
+
+# DBマイグレーション（ridgepoleを使う場合）
+bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile
+# ※ ridgepole を使わない場合は ↓ のコマンドに変更
+# bundle exec rails db:migrate
