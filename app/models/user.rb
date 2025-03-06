6c3015c09727c4ab9629edf1db76dbf63319ec9e
Seiichi Miyawaki <380keyseiichi@gmail.com>
2025-03-06 19:31:36 +0900

    bbb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   has_many :plans, dependent: :destroy
-  belongs_to :plan, optional: true  # `plan_id` を必須ではなくする
+  belongs_to :plan, optional: true
   belongs_to :frequency
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :frequency_id, presence: true
+  validates :frequency_id, presence: { message: "を選択してください" }
+  validates :plan, presence: { message: "を選択してください" }
 end
