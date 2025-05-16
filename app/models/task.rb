6fa60a72886278c98eafd957f632ea362e57cec7
Seiichi Miyawaki <380keyseiichi@gmail.com>
2025-05-17 07:00:31 +0900

    task_new

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  validates :reps, numericality: { only_integer: true, greater_than: 0 }
+  validates :sets, numericality: { only_integer: true, greater_than: 0 }
+  validates :name, presence: true
 end
