896f27137ffb3df347ee7fb0619ee9fdbd2927d1
Seiichi Miyawaki <380keyseiichi@gmail.com>
2025-05-26 04:16:23 +0900

    login14

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,31 +2,30 @@ class RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
 
   def new
-    super do |resource|
-      if session["devise.google_data"].present?
-        resource.email = session["devise.google_data"]["email"]
-        resource.name = session["devise.google_data"]["name"]
-        resource.username = session["devise.google_data"]["username"]
-      end
+    super
+    if session["devise.google_data"].present?
+      @user.email = session["devise.google_data"]["email"]
+      @user.name = session["devise.google_data"]["name"]
+      @user.username = session["devise.google_data"]["username"]
     end
   end
 
   def create
     build_resource(sign_up_params)
 
-    # Google認証情報がある場合は設定
-    if session["devise.google_data"].present?
+    # Google認証情報がある場合は設定（カラムの存在を確認）
+    if session["devise.google_data"].present? && 
+       User.column_names.include?('provider') && 
+       User.column_names.include?('uid')
       resource.provider = session["devise.google_data"]["provider"]
       resource.uid = session["devise.google_data"]["uid"]
     end
 
     if resource.save
-      # Google認証情報をクリア
       session["devise.google_data"] = nil
-      
       flash[:notice] = "ユーザー登録が完了しました！"
       sign_up(resource_name, resource)
-      redirect_to after_sign_up_path_for(resource)
+      redirect_to mypage_path
     else
       flash.now[:alert] = resource.errors.full_messages.join("、")
       render :new, status: :unprocessable_entity
@@ -39,8 +38,16 @@ class RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :name, :frequency_id, :plan_id])
   end
 
+  def sign_up_params
+    base_params = [:username, :email, :password, :password_confirmation, :plan_id, :frequency_id, :name]
+    # providerとuidカラムが存在する場合のみ許可
+    if User.column_names.include?('provider') && User.column_names.include?('uid')
+      base_params += [:provider, :uid]
+    end
+    params.require(:user).permit(base_params)
+  end
+
   def after_sign_up_path_for(resource)
-    session["devise.google_data"] = nil
-    root_path
+    mypage_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -89,6 +89,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_25_160519) do
     t.string "provider"
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
